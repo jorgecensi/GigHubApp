ad11e3228c82c6b71f8902373e9afe91b52f3257
jorgecensi <jorgecensi@gmail.com>
2019-02-06 13:10:16 +1000

    Initial check-in of GigHubApp

---
+++ GigHubApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using GigHubApp.Models;
+
+namespace GigHubApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
